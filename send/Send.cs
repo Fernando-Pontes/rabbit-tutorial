01c412974212eb3c45f8391517bead9f22656a0b
Fernando França Pontes <franca_fp@hotmail.com>
2022-04-08 16:58:32 -0300

    Tutorial-Rabbit

---
+++ send/Send.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitTutorial
+{
+    public class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = "{ message : 'Hello World'}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "hello",
+                                        basicProperties: null,
+                                        body: body);
+
+                    Console.WriteLine($"[x] Sent {message}");
+
+                }
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
